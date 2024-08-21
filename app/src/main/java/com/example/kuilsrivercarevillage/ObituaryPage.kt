73605abdb4980d2bf3a9b72f5ca00aa1014f313d
Nathan <nathan@just.co.za>
2024-08-21 08:23:27 +0200

    Kuilsriver Care Village Test 4

---
+++ app/src/main/java/com/example/kuilsrivercarevillage/ObituaryPage.kt
@@ -0,0 +1,11 @@
+package com.example.kuilsrivercarevillage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ObituaryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_obituary_page)
+    }
+}
